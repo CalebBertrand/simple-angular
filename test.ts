9d247ea9d4ae9afdd80e9f57fdd2d6156cbb04a1
Caleb <developer.caleb@proton.me>
2025-08-30 14:59:48 -0500

    fixed self closing element parsing

--- test.ts
+++ test.ts
@@ -4,19 +4,22 @@ import { bootstrapApplication } from './src/main';
 const rootElement = document.getElementById('app')!;
 
 @Component({
-    selector: 'main',
-    template: `
+  selector: 'main',
+  template: `
         <h1>Hello World!</h1>
         <p>{{ this.text }}</p>
-        <p>And this is in uppercase: {{ this.text.toUpperCase(); }}</p>
+        <p>
+          And this is in uppercase: <br />
+          {{ this.text.toUpperCase(); }}
+        </p>
         <button (click)="this.addExclamation()">Make this exciting</button>
     `,
 })
 class MainComponent {
-    text = 'This is some bound text';
-    addExclamation() {
-        this.text = this.text + '!';
-    }
+  text = 'This is some bound text';
+  addExclamation() {
+    this.text = this.text + '!';
+  }
 }
 
 bootstrapApplication(rootElement, MainComponent);
